e3189a70a16bcd5f4c5a10895b66f92cc9a84278
Asser Elshafey <elshafeyasser@yahoo.com>
2023-10-30 23:43:40 +0200

    HTTP access control

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,26 +1,34 @@
 #!/usr/bin/python3
-"""Module that initiates flask app"""
-
+"""Endpoint (route) will be to return the status of your API"""
 import os
 from flask import Flask
 from models import storage
 from api.v1.views import app_views
+from flask_cors import CORS
 
+
+# creating a Flask app
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
+
 
 app.register_blueprint(app_views, url_prefix="/api/v1")
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return {"error": "Not found"}, 404
+
+
+@app.errorhandler(400)
+def page_not_found(e):
+    message = e.description
+    return message, 400
+
+
 @app.teardown_appcontext
 def close(ctx):
-    """Method that closes on error"""
     storage.close()
-
-
-@app.errorhandler(404)
-def not_found(e):
-    """Method that returns an error"""
-    return {"error": "Not found"}, 404
 
 
 if os.getenv("HBNB_API_HOST"):
@@ -33,5 +41,6 @@ if os.getenv("HBNB_API_PORT"):
 else:
     port = 5000
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(host=host, port=port, threaded=True)
